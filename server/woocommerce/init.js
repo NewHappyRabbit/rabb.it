d2c405de7ba7311dc655aaefdb3bfe06c7630806
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2024-09-28 18:03:23 +0300

    updated some stuff for woocommerce

--- server/woocommerce/init.js
+++ server/woocommerce/init.js
@@ -1,4 +1,5 @@
 import { WooCommerce } from "../config/woocommerce.js";
+import { Product } from "../models/product.js";
 import { WooCreateCategoriesINIT } from "./categories.js";
 import { WooCheckProductAttributesINIT, WooCreateProductsINIT } from "./products.js";
 
@@ -8,7 +9,22 @@ export async function FirstInitWooCommerce() {
     console.log("Starting WooCommerce initialization...")
 
     // Check if all products attributes exist in WooCommerce, if not - create them
-    WooCheckProductAttributesINIT();
-    WooCreateCategoriesINIT();
-    WooCreateProductsINIT();
+    // WooCheckProductAttributesINIT();
+    // WooCreateCategoriesINIT();
+    // Delete old products
+    /* await WooCommerce.get("products?fields=id", { per_page: 100 }).then(async (response) => {
+        const products = response.data;
+
+        await WooCommerce.post("products/batch", { delete: products.map(p => p.id) }).then(async (response) => {
+            // Success
+            console.log(response);
+            console.log("Batch deleted products successfully in WooCommerce!")
+        }).catch((error) => {
+            // Invalid request, for 4xx and 5xx statuses
+            console.error("Response Status:", error.response.status);
+            console.error("Response Headers:", error.response.headers);
+            console.error("Response Data:", error.response.data);
+        });
+        }) */
+    // WooCreateProductsINIT();
 }
