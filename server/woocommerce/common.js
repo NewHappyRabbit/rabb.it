f93b6a9d68ed4d532219c26d79a1bc89532c5aee
NewHappyRabbit <newhappyrabbit3@gmail.com>
2024-10-31 14:20:06 +0200

    updated error logger

--- server/woocommerce/common.js
+++ server/woocommerce/common.js
@@ -4,9 +4,12 @@ export async function retry(fn, timesLeft = 10, timeout = 5000) {
         await fn();
     } catch (error) {
         // Invalid request, for 4xx and 5xx statuses
-        console.error("Response Status:", error.response.status);
-        console.error("Response Headers:", error.response.headers);
-        console.error("Response Data:", error.response.data);
+        if (error?.response?.status) {
+            console.error("Response Status:", error.response.status);
+            console.error("Response Headers:", error.response.headers);
+            console.error("Response Data:", error.response.data);
+        } else console.error(error);
+
         await new Promise(resolve => setTimeout(resolve, timeout));
         retry(fn, timesLeft - 1);
     }
